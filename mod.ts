1590fa278303cde292dea861e6d94b38a057a1e0
Chuan Xu <xchuan@gmail.com>
2024-09-04 11:00:44 +0800

    add json backup

--- mod.ts
+++ mod.ts
@@ -1,30 +1,85 @@
 #!/usr/bin/env -S deno run --unstable --allow-net --allow-read --allow-write --import-map=import_map.json mod.ts
 import { IResponse, IImg, IArticle } from "./types.ts";
-import { createArticle } from "./utils.ts";
+import { createArticle, getWeekNumber, writeJson } from "./utils.ts";
+import * as Minio from "minio";
+import { existsSync } from "std/fs/exists.ts";
+import moment from 'moment';
 
 const NEWS_API = `https://api-one-wscn.awtmt.com/apiv1/content/lives?channel=global-channel&client=pc&limit=20&first_page=true&accept=live%2Cvip-live`;
 
-const response = await fetch(NEWS_API);
+const minioClient = new Minio.Client({
+  endPoint: 'cone.xhashao.top',
+  port: 443,
+  useSSL: true,
+  accessKey: 'vvjZPE9sc6KdrLV625nW',
+  secretKey: 'hXiQTWle3iIFRWcP9w9V2Q2M8UihstZJsWhER7UQ',
+})
 
-if (!response.ok) {
-  console.error(response.statusText);
-  Deno.exit(-1);
+const getBuckets = async() =>{
+  try {
+    const buckets = await minioClient.listBuckets();
+    //minioClient.fPutObject
+    console.log('Success', buckets)
+  } catch (err) {
+    console.log(err.message)
+  }
 }
 
-const { data }: IResponse = await response.json();
-const articles = data.items;
 const allArchiveText:string[] = [];
+const weeknumber = getWeekNumber(new Date());
+const outdir = `./news/${weeknumber[0]}/w${weeknumber[1]}`;
+const minnumber = Number(moment().format('YYYYMMDDHHmm'));
+const jsonfname = minnumber - minnumber%5;
 
-if(articles.length > 0) {
 
-  for (let index = 0; index < articles.length; index++) {
-    const articleInfo: IArticle = articles[index];
-    // 更新 archives
-    const archiveTextSingle = createArticle(articleInfo);
-    allArchiveText.push(archiveTextSingle);
+let dirExists = existsSync(outdir);
+let fileExists = existsSync(`${outdir}/${jsonfname}.json`);
+console.log(dirExists,fileExists);// returns boolean
+
+if(!dirExists){
+  await Deno.mkdir(outdir, { recursive: true });
+}
+const uploadJson = (filepath:string) => {
+  const metaData = {
+    'Content-Type': 'application/json',
+  }
+  minioClient.fPutObject('nchome', `${jsonfname}.json`, filepath, metaData, function (err, objInfo) {
+    if (err) {
+      return console.log(err)
+    }
+    console.log('Success', objInfo)
+  })
+}
+
+if(!fileExists){
+  const response = await fetch(NEWS_API);
+  const { data }: IResponse = await response.json();
+  const articles = data.items;
+
+  if (!response.ok) {
+    console.error(response.statusText);
+    Deno.exit(-1);
   }
 
+  if(articles.length > 0) {
 
+    for (let index = 0; index < articles.length; index++) {
+      const articleInfo: IArticle = articles[index];
+      // 更新 archives
+      const archiveTextSingle = createArticle(articleInfo);
+      allArchiveText.push(archiveTextSingle);
+    }
   
-  await Deno.writeTextFile("./articles.html", allArchiveText.join("\n"), { append: true });
+    console.log(weeknumber);
+    //moment().format('YYYYMMDDHHmm');
+    writeJson(`${outdir}/${jsonfname}.json`,data);  
+    uploadJson(`${outdir}/${jsonfname}.json`);
+    await Deno.writeTextFile("./articles.html", allArchiveText.join("\n"), { append: true });
+  }
+}else{
+  uploadJson(`${outdir}/${jsonfname}.json`);
 }
+
+//getBuckets();
+
+
--- utils.ts
+++ utils.ts
@@ -40,5 +40,37 @@ function timestampToLocalDate(timestamp) {
 }
 
 export function createArticle(image: IArticle): string {
-  return `${image.content_text} | ![${image.author.display_name}] ${timestampToLocalDate(image.display_time)}`;
+  const author = image.author ? image.author.display_name : "Unknown";
+  return `${image.content_text} | ![${author}] ${timestampToLocalDate(image.display_time)}`;
+}
+
+export function getWeekNumber(d: Date) {
+  // Copy date so don't modify original
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  // Set to nearest Thursday: current date + 4 - current day number Make
+  // Sunday's day number 7
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  // Get first day of year
+  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  // Calculate full weeks to nearest Thursday
+  const weekNo = Math.ceil(
+    ((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7
+  );
+
+  const weekStartDate = new Date(d.getTime());
+  weekStartDate.setUTCDate(weekStartDate.getUTCDate() - 3);
+
+  const weekEndDate = new Date(d.getTime());
+  weekEndDate.setUTCDate(weekEndDate.getUTCDate() + 3);
+
+  return [d.getUTCFullYear(), weekNo, weekStartDate, weekEndDate] as const;
+}
+
+export function writeJson(path: string, data: object): string {
+  try {
+    Deno.writeTextFileSync(path, JSON.stringify(data));
+    return "Written to " + path;
+  } catch (e) {
+    return e.message;
+  }
 }
